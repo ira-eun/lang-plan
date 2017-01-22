8b5a57780ef901526c29f08b8f86d8fae6bb8201
ira-eun <ira.eun@gmail.com>
2017-01-22 23:58:18 +0900

    Update

--- week02-hw/studyhw/study/urls.py
+++ week02-hw/studyhw/study/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^tool/', include('tool.urls')),
+    url(r'^', include('tool.urls')),
     url(r'^admin/', admin.site.urls),
 ]
